17f4ea5bf26e4ace9c340692859f6e8b27ba280c
Lucas William <lucaswilliameufrasio@gmail.com>
2021-07-01 19:12:36 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,205 @@
+import React from "react";
+import styles from "./AdvisorManageTeachers.module.scss";
+
+const AdvisorManageTeachers = () => {
+  return (
+    <div className={styles.AdvisorManageTeachers}>
+      <p className={styles.minisigaa}>MiniSIGAA</p>
+      <div className={styles.teacherList}>
+        <div className={styles.button}>
+          <p className={styles.adicionarProfessor}>
+            Adicionar Professor
+          </p>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+        <div className={styles.teacherItem}>
+          <div className={styles.flexWrapperTwentySix}>
+            <p className={styles.johnDoe}>John Doe</p>
+            <p className={styles.registro20219897898909}>
+              Registro: 20219897898909
+            </p>
+          </div>
+          <div className={styles.flexWrapperTwentySeven}>
+            <p className={styles.ativo}>Ativo</p>
+            <p className={styles.registro20219897898909}>
+              johndoe&#64;example.com
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.flexWrapperTwentySeven}>
+        <p className={styles.johnDoe}>Master Advisor</p>
+        <p className={styles.sair}>Sair</p>
+      </div>
+    </div>
+  );
+};
+
+export default AdvisorManageTeachers;
